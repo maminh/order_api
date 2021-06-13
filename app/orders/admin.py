b4ce7bb3e3c328b02d24989738f04d1816cdec58
Amin Hosseini <m.amin.h.h@gmail.com>
2021-06-14 02:06:36 +0430

    import export added for order

--- app/orders/admin.py
+++ app/orders/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from products.models import Product
 
-# Register your models here.
+from . import models
+
+
+class ProductInline(admin.StackedInline):
+    model = Product
+    fields = ('id', 'name', 'price')
+    extra = 0
+    show_change_link = True
+
+
+class OrderResource(resources.ModelResource):
+    class Meta:
+        model = models.Order
+        fields = ('id', 'customer_name', 'customer_city', 'customer_country', 'longitude', 'latitude',)
+        export_order = fields
+
+
+@admin.register(models.Order)
+class OrderAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'customer_name', 'customer_city', 'customer_country', 'longitude', 'latitude',)
+    fields = ('id', 'customer_name', 'customer_city', 'customer_country', 'products', 'longitude', 'latitude',)
+    resource_class = OrderResource
